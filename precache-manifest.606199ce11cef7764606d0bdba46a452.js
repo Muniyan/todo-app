834c61fdd34578749b9425f5042b540713f0049c
Muniyan <50628045+Muniyan@users.noreply.github.com>
2020-06-22 13:45:43 +0530

    Updates

---
+++ precache-manifest.606199ce11cef7764606d0bdba46a452.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1793ff197496097a64d54a12463127f",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "d8250b744a45d9e574d7",
+    "url": "/todo-app/static/css/main.f2fbdd54.chunk.css"
+  },
+  {
+    "revision": "0aff0bbec90e9e71c763",
+    "url": "/todo-app/static/js/2.7fdc2008.chunk.js"
+  },
+  {
+    "revision": "d8250b744a45d9e574d7",
+    "url": "/todo-app/static/js/main.637b1ece.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
