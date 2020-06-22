5cffa9e707513dd6a0165f5606752022952bb4a6
Muniyan <50628045+Muniyan@users.noreply.github.com>
2020-06-22 20:26:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
 
   renderRedirect = () => {
     let search = window.location.search;
-    console.log("search=================  ", window.location.origin, window.location.pathname, window.location.search);
     if(search.indexOf("?p=") !== -1) {
       let url = window.location.search.slice(3);
+      console.log("url=================== ", window.location.pathname, url);
       return <Redirect to={url} />
     }
   }
