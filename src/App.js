1007441563dbf9bfd1b3b6d17da132a38aff3c04
Muniyan <50628045+Muniyan@users.noreply.github.com>
2020-06-28 14:53:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,31 +7,20 @@ import NotFound from "./components/notFound";
 
 class App extends Component {
 
-  renderRedirect = () => {
-    let search = window.location.search;
-    if(search.indexOf("?p=") !== -1) {
-      let url = window.location.pathname;
-      // let url = window.location.pathname + window.location.search.slice(3);
-      console.log("url new=================== ", url);
-      return <Redirect to={url} />
-    }
-  }
-
   render() {
     return (
       <div className="App">
-        { this.renderRedirect() }
         <header className="App-header w100">
           <span className="App-icon fleft" />
-          <Link to="/todo/" className="App-name fleft cpointer">
+          <Link to="/" className="App-name fleft cpointer">
             To Do App
           </Link>
         </header>
 
         <div className="App-container fleft">
           <Switch>
-            <Route exact path="/todo/" component={Todo} />
-            <Route path="/todo/*" component={NotFound} />
+            <Route exact path="/" component={Todo} />
+            <Route path="/*" component={NotFound} />
           </Switch>
         </div>
       </div>
